d1924392efad0d8987460660eb58348ff3a00413
CLleese <CLleese@users.noreply.github.com>
2020-06-11 00:58:50 +0300

    Выполнен 3 этап!
    Добавил пару валют и возможность конвертировать все друг в друга. Отказался от кнопки смены активных валют

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,66 +7,118 @@ import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
+
 
     @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var course: Double = 74.0
-        var icon:String = "$"
+        var right_course = 0.0 //все курсы относительно рубля
+        var left_course = 0.0 //не знаю как реализовать lateinit в данной ситуации
+        var icon_right: String = "$"
+        val toast = Toast.makeText(getApplicationContext(),
+            "Введите желаемое количество валюты", Toast.LENGTH_SHORT)
 
         convert.setOnClickListener {
-            var money = textInputEditText.text.toString().toDouble()
-            dollars.setText("${money/course} ${icon}")
-        }
 
-
-        refresh.setOnClickListener{
-
-            if (icon == "$" && course == 74.0){
-                textInputEditText.setHint("Введите количество долларов")
-                icon = "рублей"
+            if (textInputEditText.length() == 0){
+                toast.show()
             }else{
-                textInputEditText.setHint("Введите количество рублей")
-                icon = "$"
+                var course = left_course / right_course
+                var money = textInputEditText.text.toString().toDouble()
+                dollars.setText("${Math.round(money * course * 100.0) / 100.0} ${icon_right}")
+
             }
 
-            course = 1/course
-            textInputEditText.setText("")
         }
 
 
-
-        //Выпадающий список валют
-        var values = arrayOf("dollar")
+        //Выпадающий список валют справа
+        var values = arrayOf("рубль", "доллар", "евро", "иена")
 
         currency.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values)
 
-        currency.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+        currency.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
 
             }
 
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
 
-                when(id){
+                when (id) {
                     0L -> {
-                        course = 74.0
-                        icon = "$"
+                        right_course = 1.0
+                        icon_right = "рублей"
+                    }
+                    1L -> {
+                        right_course = 74.0
+                        icon_right = "$"
                     }
 
+                    2L -> {
+                        right_course = 80.53
+                        icon_right = "евро"
+                    }
+                    3L -> {
+                        right_course = 0.64
+                        icon_right = "иен"
+                    }
 
                 }
             }
         }
 
-         //Реализация подсказки в поле ввода
-    }
 
+        //Выпадающий список слева
+        currency2.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values)
+
+
+        currency2.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                when (id) {
+                    0L -> {
+                        left_course = 1.0
+                        textInputEditText.setHint("Введите количество рублей")
+                    }
+                    1L -> {
+                        left_course = 74.0
+                        textInputEditText.setHint("Введите количество долларов")
+                    }
+                    2L -> {
+                        left_course = 80.0
+                        textInputEditText.setHint("Введите количество евро")
+                    }
+                    3L -> {
+                        left_course = 0.64
+                        textInputEditText.setHint("Введите количество иен")
+
+                    }
+
+
+                }
+            }
+
+        }
+    }
 }
 
 
